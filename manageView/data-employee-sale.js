59b563a7de1d40a98c2b1fbb6822d62673e57357
Khanh-12-felix <hathaikhanh0@gmail.com>
2025-05-21 11:31:42 +0700

    add data test

---
+++ manageView/data-employee-sale.js
@@ -0,0 +1,37 @@
+const Sale= [
+    {
+        "manv": "NV006",
+        "tennv": "Hà Khánh",
+        "date": "12-12-2005",
+        "role": "seller",
+        "hopDong": "BHYT"         
+    },
+    {
+        "manv": "NV007",
+        "tennv": "Nguyễn Bình",
+        "date": "31-12-2005",
+        "role": "seller",
+        "hopDong": "BHNT"         
+    },
+    {
+        "manv": "NV008",
+        "tennv": "Phan Đại",
+        "date": "14-05-2005",
+        "role": "seller",
+        "hopDong": "BHOT"         
+    },
+    {
+        "manv": "NV009",
+        "tennv": "Nguyễn Chí",
+        "date": "15-05-2005",
+        "role": "seller",
+        "hopDong": "BHXM"         
+    },
+    {
+        "manv": "NV010",
+        "tennv": "Hà Cường",
+        "date": "20-05-2005",
+        "role": "seller",
+        "hopDong": "BHNT"         
+    }
+];
