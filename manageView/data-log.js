244dfd46e7b4d689ed0f745642d9b373f8a97371
Khanh-12-felix <hathaikhanh0@gmail.com>
2025-05-21 14:28:21 +0700

    log management

---
+++ manageView/data-log.js
@@ -0,0 +1,42 @@
+const Log = [
+    {
+        "LogID": "LOG001",
+        "UserType": "InsuranceContracts",
+        "ActionType": "INSERT",
+        "ChangeByUserID": "NV010",
+        "Detail": "Tạo hợp đồng mới: Số hợp đồng = 202310..., Người đảm bảo...", 
+        "ChangeDate": "2023-10-27 10:35:00.456"         
+    },
+    {
+        "LogID": "LOG002",
+        "UserType": "Employee",
+        "ActionType": "INSERT",
+        "ChangeByUserID": "NV110",
+        "Detail": "Tạo tài khoản nhân viên mới: NV110", 
+        "ChangeDate": "2024-10-27 10:35:00.456"         
+    },
+    {
+        "LogID": "LOG003",
+        "UserType": "Employee",
+        "ActionType": "DELETE",
+        "ChangeByUserID": "Admin",
+        "Detail": "Xoá tài khoản nhân viên: NV110", 
+        "ChangeDate": "2024-12-12 11:42:56.456"         
+    },
+    {
+        "LogID": "LOG004",
+        "UserType": "InsuranceContracts",
+        "ActionType": "UPDATE",
+        "ChangeByUserID": "Admin",
+        "Detail": "Cập nhật hợp đồng: Số hợp đồng = 177652..., Người đảm bảo...", 
+        "ChangeDate": "2023-10-27 10:35:00.456"         
+    },
+    {
+        "LogID": "LOG005",
+        "UserType": "InsuranceContracts",
+        "ActionType": "UPDATE",
+        "ChangeByUserID": "NV012",
+        "Detail": "Cập nhật hợp đồng mới: Số hợp đồng = 107852..., Người đảm bảo...", 
+        "ChangeDate": "2023-10-27 10:35:00.456"         
+    }
+]
