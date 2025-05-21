05e5b88af29b38d176c180dad83d5880c213589b
Khanh-12-felix <hathaikhanh0@gmail.com>
2025-05-21 12:25:04 +0700

    update add employee

--- manageView/data-employee-sale.js
+++ manageView/data-employee-sale.js
@@ -1,37 +1,32 @@
-const Sale= [
+const Sale = [
     {
         "manv": "NV006",
         "tennv": "Hà Khánh",
         "date": "12-12-2005",
-        "role": "seller",
-        "hopDong": "BHYT"         
+        "role": "seller"       
     },
     {
         "manv": "NV007",
         "tennv": "Nguyễn Bình",
         "date": "31-12-2005",
-        "role": "seller",
-        "hopDong": "BHNT"         
+        "role": "seller",        
     },
     {
         "manv": "NV008",
         "tennv": "Phan Đại",
         "date": "14-05-2005",
-        "role": "seller",
-        "hopDong": "BHOT"         
+        "role": "seller"         
     },
     {
         "manv": "NV009",
         "tennv": "Nguyễn Chí",
         "date": "15-05-2005",
-        "role": "seller",
-        "hopDong": "BHXM"         
+        "role": "seller"        
     },
     {
         "manv": "NV010",
         "tennv": "Hà Cường",
         "date": "20-05-2005",
-        "role": "seller",
-        "hopDong": "BHNT"         
+        "role": "seller"        
     }
 ];
---
+++ manageView/data-employee-supervisor.js
@@ -0,0 +1,37 @@
+const Supervisor = [
+    {
+        "manv": "NV011",
+        "tennv": "Khánh",
+        "date": "12-12-2005",
+        "role": "supervisor",
+        "hopDong": "BHYT"         
+    },
+    {
+        "manv": "NV012",
+        "tennv": "Bình",
+        "date": "31-12-2005",
+        "role": "supervisor",
+        "hopDong": "BHNT"         
+    },
+    {
+        "manv": "NV013",
+        "tennv": "Đại",
+        "date": "14-05-2005",
+        "role": "supervisor",
+        "hopDong": "BHOT"         
+    },
+    {
+        "manv": "NV014",
+        "tennv": "Chí",
+        "date": "15-05-2005",
+        "role": "supervisor",
+        "hopDong": "BHXM"         
+    },
+    {
+        "manv": "NV015",
+        "tennv": "Cường",
+        "date": "20-05-2005",
+        "role": "supervisor",
+        "hopDong": "BHNT"         
+    }
+];
