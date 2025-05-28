7dfe58f920e7d0bc1dd1430edd701795b348797d
Thai Khanh Southside <hathaikhanh0@gmail.com>
2025-05-28 13:27:05 +0700

    form update

--- InsuranceManageByEmploy/data-test.js
+++ InsuranceManageByEmploy/data-test.js
@@ -1,37 +1,22 @@
 const Contact = [
     {
         "maHD": "HD001",
-        "Bảo hiểm nhân thọ": "Bảo hiểm nhân thọ",
-        "nguoiThuHuong": "Nguyễn Văn A",
-        "ngayKiKet": "2023-01-15",
-        "ngayKetThuc": "2033-01-15"              
+        "Bảo hiểm nhân thọ": "Bảo hiểm nhân thọ"        
     },
     {
         "maHD": "HD002",
-        "Bảo hiểm nhân thọ": "Bảo hiểm y tế",
-        "nguoiThuHuong": "Trần Thị B",
-        "ngayKiKet": "2024-05-10",
-        "ngayKetThuc": "2029-05-10"
+        "Bảo hiểm nhân thọ": "Bảo hiểm y tế"
     },
     {
         "maHD": "HD003",
-        "Bảo hiểm nhân thọ": "Bảo hiểm nhân thọ",
-        "nguoiThuHuong": "Nguyễn Văn A",
-        "ngayKiKet": "2023-01-15",
-        "ngayKetThuc": "2033-01-15"              
+        "Bảo hiểm nhân thọ": "Bảo hiểm nhân thọ"        
     },
     {
         "maHD": "HD004",
-        "Bảo hiểm nhân thọ": "Bảo hiểm y tế",
-        "nguoiThuHuong": "Trần Thị B",
-        "ngayKiKet": "2024-05-10",
-        "ngayKetThuc": "2029-05-10"
+        "Bảo hiểm nhân thọ": "Bảo hiểm y tế"
     },
     {
         "maHD": "HD005",
-        "Bảo hiểm nhân thọ": "Bao hiểm nhân thọ",
-        "nguoiThuHuong": "Nguyễn Văn A",
-        "ngayKiKet": "2023-01-15",
-        "ngayKetThuc": "2033-01-15"              
+        "Bảo hiểm nhân thọ": "Bao hiểm nhân thọ"         
     }
 ];
